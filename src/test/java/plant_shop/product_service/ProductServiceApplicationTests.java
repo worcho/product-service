cd6d9fd83019a4389e1b8e3b0c7de17de40c3a80
dvirisha <virishapkov@media-it-services.de>
2025-08-24 12:54:55 +0300

    Initial commit

---
+++ src/test/java/plant_shop/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package plant_shop.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
