77980366773a9abfbaddabbcd9bdcd293988ff76
worcho <worcho95@gmail.com>
2025-08-25 10:49:34 +0300

    getter and setter removed

--- src/main/java/plant_shop/product_service/models/ProductEntity.java
+++ src/main/java/plant_shop/product_service/models/ProductEntity.java
@@ -26,27 +26,4 @@ public class ProductEntity {
     private String name;
     private BigDecimal price;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
